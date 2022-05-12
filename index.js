e3beef581414438d74e3aab8c66807c475c00c3c
Niraj Singh <102036558+Starniraj@users.noreply.github.com>
2022-05-12 20:33:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,16 @@ set.add("e");
 set.add("c");
 set.add("b");
 set.add("f");
+   
+   or 
+   
+// var str = ["a","b","c","a","d","e","e","c","f","b"];
+var str;
+// console.log("enter string:");
+str = prompt("enter a string:");
 
+let a = new Set(str);
+console.log(...a);
 
 console.log(set)
 
@@ -43,3 +52,40 @@ uniqueCount = ["a","b","c","a","d","e","e","c","f","b"];
 var count = {};
 uniqueCount.forEach(function(x) { count[x] = (count[x]||0) + 1;});
 console.log(count);
+
+
+or   
+
+var s="abcadeecfb";
+function prCharWithFreq(s)
+{
+// Store all characters and
+// their frequencies in dictionary
+var d = new Map();
+
+s.split('').forEach(element => {
+ 
+    if(d.has(element))
+    {
+        d.set(element, d.get(element)+1);
+    }
+    else
+        d.set(element, 1);
+});
+
+// Print characters and their
+// frequencies in same order
+// of their appearance
+
+s.split('').forEach(element => {
+// Print only if this
+// character is not printed
+// before
+if(d.has(element) && d.get(element)!=0)
+{
+  document.write( element + d.get(element) + " ");
+  d.set(element, 0);
+}
+});
+}
+prCharWithFreq(s);
